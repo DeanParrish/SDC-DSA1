30bed7ee132555ec142217912f1e09a4526db132
Dean Parrish <parrish.de@gmail.com>
2023-06-12 19:40:47 -0400

    Bubble Sort of 2d array

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       
+
         CapitalArray capitalArray = new CapitalArray();
 
         String userInput = capitalArray.GetUserInput("Enter a capital...");
@@ -8,13 +8,20 @@ public class App {
 
         if (boolIsCapitalInList == true) {
             System.out.println("User is correct!");
-        }else{
+        } else {
             System.out.println("User is incorrect.");
         }
 
         String[][] arrCapital = capitalArray.GetCapitalArray();
 
         for (String[] string : arrCapital) {
+            System.out.println(string[0] + " - " + string[1]);
+        }
+
+        String[][] sortedCapitalArray = capitalArray.BubbleSort(arrCapital);
+        System.out.println("-----------Results after bubble sort:----------");
+
+        for (String[] string : sortedCapitalArray) {
             System.out.println(string[0] + " - " + string[1]);
         }
 
