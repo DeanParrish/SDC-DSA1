1aa243a43ea932fb1daba5d6ffd8652c4345d37f
Dean Parrish <parrish.de@gmail.com>
2023-06-13 20:10:58 -0400

    Rebase with hashmap, treemap

--- src/App.java
+++ src/App.java
@@ -1,40 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        //init new instance of Capital Array object
-        CapitalArray capitalArray = new CapitalArray();
+        //init new instance of Capital Hashmap object
+        CapitalHashMap objCapitalHashMap = new CapitalHashMap();
+        
+        Map<String,String> hashStateCapital = objCapitalHashMap.GetStateCapitalHM();
+        //display the contents of hashmap
+        hashStateCapital.forEach((key, values)
+        -> System.out.println(key + " - " +values));
 
-        //Prompt user for capital, return their response
-        String userInput = capitalArray.GetUserInput("Enter a capital...");
-        //Call for capital check method
-        boolean boolIsCapitalInList = capitalArray.IsCapitalInList(userInput);
+        //tree map for sorting by key
+        Map<String, String> treeStateCapital = objCapitalHashMap.GetSortedTreeMap();
+        System.out.println("-----Tree Map -----");
+        //iterate over treemap to show sort
+        treeStateCapital.forEach((key, values)
+        -> System.out.println(key + " - " +values));
 
-        //handle displaying if user is correct
-        if (boolIsCapitalInList == true) {
-            System.out.println("User is correct!");
-        } else {
-            System.out.println("User is incorrect.");
+        //start game of entering state and returning capital
+        System.out.println("-----Start Game -----");
+        String strCapital = objCapitalHashMap.GetCapitalForStateGame();
+        if (strCapital != null) {
+            System.out.println(strCapital);
+        }else{
+            System.out.println("Not Found");
         }
-
-        //returns the array
-        String[][] arrCapital = capitalArray.GetCapitalArray();
-
-        //prints the array
-        for (String[] string : arrCapital) {
-            System.out.println(string[0] + " - " + string[1]);
-        }
-
-        //call for bubble sorting method
-        String[][] sortedCapitalArray = capitalArray.BubbleSort(arrCapital);
-        //separator for pre and post bubble sort
-        System.out.println("-----------Results after bubble sort:----------");
-        //prints array after bubble sort
-        for (String[] string : sortedCapitalArray) {
-            System.out.println(string[0] + " - " + string[1]);
-        }
-        //call for capital game that prompts user to enter the capital for state
-        //then displays the correct count
-        capitalArray.CapitalGame();
-
     }
 }
